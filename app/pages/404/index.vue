e5c6d365b7ba68954be1090baa88df0d2a250e21
Friday <peterkimzz69@gmail.com>
2018-02-20 13:19:18 +0900

    Added 404 page. Added company brochure. Added pricing page.

---
+++ app/pages/404/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-container">
+    <div class="body-contents">
+      Sorry, page not found.
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: 'Page not found',
+        link: [
+          { hid: 'canonical', rel: 'canonical', href: `https://www.factoryhunt.com/404` }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import '~assets/css/index';
+</style>
