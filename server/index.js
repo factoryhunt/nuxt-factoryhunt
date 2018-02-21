452d9b2383e715fd6e880dbf19ef9179cd02d92f
Friday <peterkimzz69@gmail.com>
2018-02-21 11:04:20 +0900

    Integrated express-session with redis

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ process.env.DEBUG = 'nuxt:*'
 const app = require('express')()
 
 const session = require('express-session')
-const connectRedis = require('connect-redis')
-const RedisStore = connectRedis(session)
+const redis = require('./middleware/redis/.config')(session)
 const nuxt = require('./middleware/nuxt')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
@@ -14,27 +13,10 @@ const api = require('./api')
 const CONFIG = require('./api/.config')
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 3000
-const sess = {
-  resave: false,
-  saveUninitialized: false,
-  secret: CONFIG.sessionKey,
-  name: 'redisSession',
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-  store: new RedisStore({ url: '레디스 호스팅 주소', logErrors: true }),
-}
 
 // Middlewares
 app.set('jwt-token', CONFIG.jwtSecret)
-app.use(session(sess))
-// app.use(session({
-//   secret: CONFIG.sessionKey,
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { maxAge: 60 * 60 * 3600 }
-// }))
+app.use(session(redis))
 app.use(morgan('dev'))
 app.use(cors())
 app.use(cookieParser())
