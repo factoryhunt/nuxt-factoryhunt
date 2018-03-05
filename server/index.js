b2cc6d73065ea7a9d5414bb72c6946a38d5395b2
Friday <peterkimzz69@gmail.com>
2018-03-05 10:58:40 +0900

    Installed npm package express-http-to-https

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 process.env.DEBUG = 'nuxt:*'
 const app = require('express')()
 
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 const session = require('express-session')
 const redis = require('./middleware/redis/.config')(session)
 const nuxt = require('./middleware/nuxt')
@@ -10,19 +11,20 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const api = require('./api')
 
-const CONFIG = require('./api/.config')
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 3000
 
 // Middlewares
-app.set('jwt-token', CONFIG.jwtSecret)
 app.use(session(redis))
 app.use(morgan('dev'))
 app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// APIs
 app.use('/api', api)
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/]))
 app.use(nuxt.render)
 
 app.listen(PORT)
