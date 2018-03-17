b22acf5cbb6e38be4b069415c3bc38308bc002ed
Friday <peterkimzz69@gmail.com>
2018-03-17 19:55:36 +0900

    removed winston

--- server/api/mysql/index.js
+++ server/api/mysql/index.js
@@ -1,15 +1,14 @@
 const mysql = require('mysql')
 const config = require('./.config')
-const { errorLogger, infoLogger } = require('../../middleware/winston')
 
 // parameters
 const connection = mysql.createConnection(config)
 
 // methods
 connection.connect((err) => {
-  if (err) return errorLogger.error('Error connecting database ... \n' + err)
+  if (err) return console.log('Error connecting database ... \n' + err)
 
-  infoLogger.info(`Database "${connection.config.database}" is connected ... \n`)
+  console.log(`Database "${connection.config.database}" is connected ... \n`)
 })
 
 module.exports = connection
--- server/middleware/elastic/index.js
+++ server/middleware/elastic/index.js
@@ -1,14 +1,13 @@
 const es = require('elasticsearch')
 const { config, ping } = require('./.config')
-const { errorLogger, infoLogger } = require('../winston')
 const client = new es.Client(config)
 const host = process.env.NODE_ENV ? 'AWS' : 'localhost'
 
 // ping usually has a 3000ms timeout
 client.ping(ping, (err) => {
-  if (err) return errorLogger.error('elasticsearch cluster is down!\n')
+  if (err) return console.log('elasticsearch cluster is down!\n')
 
-  infoLogger.info(`Connected with "${host}" Elasticserach server.\n`)
+  console.log(`Connected with "${host}" Elasticserach server.\n`)
 })
 
 module.exports = client
