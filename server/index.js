245db7565268884ac62e28885ddfeaae4043fa62
Friday <peterkimzz69@gmail.com>
2018-03-19 13:44:16 +0900

    https redirect added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // process.env.DEBUG = 'nuxt:*'
 const app = require('express')()
 
-// const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 const session = require('express-session')
 const redis = require('./middleware/redis/.config')(session)
 const nuxt = require('./middleware/nuxt')
@@ -23,8 +23,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // APIs
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/]))
 app.use('/api', api)
-// app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/]))
 app.use(nuxt.render)
 
 app.listen(PORT)
