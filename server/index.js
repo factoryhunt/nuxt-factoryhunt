a92600076b9a7b0078b5ae8e58b247d027fa6bee
Friday <peterkimzz69@gmail.com>
2018-04-07 17:30:52 +0900

    add sitemap.xml.gz

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const cors = require('cors')
 const api = require('./api')
 require('./middleware/socket')(server)
 // require('./middleware/v8')
+// require('./middleware/sitemap')
 
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 3000
