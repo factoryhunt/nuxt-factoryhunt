dfe114650b084442fc22fadef09c073aabcce23c
Friday <peterkimzz69@gmail.com>
2018-04-16 15:06:05 +0900

    forcedomain port removed

--- server/middleware/forcedomain/index.js
+++ server/middleware/forcedomain/index.js
@@ -1,7 +1,6 @@
 const forcedomain = require('forcedomain')
 const options = {
   hostname: 'www.factoryhunt.com',
-  port: 3000,
   protocol: 'https',
   excludeRule: /\.amazonaws.com$/ // localhost is excluded by default
 }
