6c7df50cbfdf07b286d564eb193f2d9a0a32b6a6
Friday <peterkimzz69@gmail.com>
2018-04-25 18:01:43 +0900

    sign up wizard

--- app/layouts/wizard.vue
+++ app/layouts/wizard.vue
@@ -1,26 +1,46 @@
 <template>
   <div id="container">
     <top-alert-bar/>
+
     <div id="wizard-container">
+      
+      <!-- Left Fixed Bar -->
       <div id="left-bar">
+        <div id="brand-container">
+          <img 
+            id="brand-logo"
+            src="~assets/img/logo_orange_small.png" 
+            alt="brand-logo"/>
+          <p id="desc">Factory Hunt<br>Sign Up Wizard</p>
+        </div>
         <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
-          <li>8</li>
+          <li>Basic Company Information</li>
+          <li>Contacts & Address</li>
+          <li>Business Details</li>
+          <li>Brand Images</li>
+          <li>Certification & Awards</li>
         </ul>
       </div>
+
+      <!-- v-on:change.native="activateSaveButton" -->
+      <!-- Right Scrollable Bar -->
       <div id="right-container">
-        <nuxt/>
+        <nuxt
+          @activateSaveButton="activateSaveButton"
+          @onSave="onSaveButton"/>
       </div>
     </div>
-    <div id="bottom-bar">
-      bottom
+    <!-- Bottom Fixed Bar -->
+    <div id="bottom-bar-container">
+      <div id="bottom-bar-wrapper">
+        <button 
+          id="action-button"
+          :disabled="!toggle.canSave"
+          @click="onSaveButton">Save & Continue</button>
+        <a id="skip-button" href="/dashboard">Skip this step <i class='fa fa-angle-right' aria-hidden='true'></i></a>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -34,6 +54,21 @@ export default {
   },
   components: {
     TopAlertBar
+  },
+  data() {
+    return {
+      toggle: {
+        canSave: false
+      }
+    }
+  },
+  methods: {
+    activateSaveButton() {
+      this.toggle.canSave = true
+    },
+    onSaveButton() {
+      console.log('onSaveButton')
+    }
   }
 }
 </script>
--- app/pages/signup/step1/index.vue
+++ app/pages/signup/step1/index.vue
@@ -1,62 +1,94 @@
 <template>
   <div>
-    <ul>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-    </ul>
+    <h1>Welcome, Factory Hunt Inc.</h1>
+    <h3>In this step, we will help you to fill in your company information to connect your potential target easier.</h3>
+
+    <div id="contents">
+      <h2>Basic Company Information</h2>
+      <h3>Complete profile is crucial to increasing your visibility within our network and ensuring connections with companies from around the world.</h3>
+
+      <!-- Buyer or Supplier -> Business Type -->
+      <section>
+        <h4>Are you a buyer or supplier?</h4>
+        <select v-model="value.type">
+          <option value="" disabled>Select</option>
+          <option value="Buyer">Buyer</option>
+          <option value="Supplier">Supplier</option>
+          <option value="Buyer & Supplier">Buyer & Supplier</option>
+        </select>
+      </section>
+
+      <div v-show="value.type">
+        <section>
+          <h4>Business Type</h4>
+          <span class="required-mark">*</span>
+          <div id="scroll-container">
+            <div class="checkbox-row" v-for="(businessType, index) in businessTypes" :key="index">
+              <input
+                type="checkbox"
+                :id="businessType.value"
+                :value="businessType.value"
+                v-model="value.businessTypes"
+                @change="businessTypeUpdated"/>
+              <label 
+                :for="businessType.value">
+                {{businessType.value}}
+              </label>
+            </div>
+          </div>
+          <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident aperiam soluta eaque fuga inventore modi neque itaque laboriosam, sunt ad deserunt quos exercitationem est perferendis voluptas. Laboriosam ullam debitis eius?</h5>
+        </section>
+
+        <section>
+          <h4>Factory Hunt Domain</h4>
+          <span class="required-mark">*</span>
+          <input type="text">
+          <h5>Your website will be https://www.factoryhunt.com/sample</h5>
+        </section>
+
+        <section>
+          <h4>Website</h4>
+          <input type="text" placeholder="www.yourcompany.com">
+        </section>
+
+        <section>
+          <h4>Company Description</h4>
+          <textarea id="short-description" rows="9"></textarea>
+        </section>
+      </div>
+      
+    </div>
+
   </div>
 </template>
 
 <script>
+import business_type from '~/assets/models/business_type.json'
 export default {
   layout: 'wizard',
   data() {
-    return {}
+    return {
+      businessTypes: business_type,
+      value: {
+        type: '',
+        businessTypes: []
+      }
+    }
+  },
+  methods: {
+    onSaveButton() {
+      console.log('onSaveButton in Child Comp')
+    },
+    businessTypeUpdated() {
+      console.log(this.value.businessTypes)
+      if (this.value.businessTypes.length) this.$parent.$emit('activateSaveButton')
+    },
+    mounted() {}
   }
 }
 </script>
 
 <style lang="less" scoped>
 @import '~assets/css/index';
+@import '~assets/css/less/wizard/index';
 </style>
