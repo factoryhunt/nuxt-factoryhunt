42ab4bb5c3e199db77af72daa4bcdca99886510b
yeonhodev <yeonho.dev@gmail.com>
2018-06-04 14:43:00 +0900

    Modified pricing page

--- app/pages/pricing/index.vue
+++ app/pages/pricing/index.vue
@@ -8,38 +8,46 @@
       <!-- Starter -->
       <section class="pricing-table">
         <h2 class="table-title">{{ $t('pricing.starter') }}</h2>
-        <p class="table-pricing">$ 0</p>
+        <p class="table-pricing">$0</p>
         <ul>
-          <li>Upload product unlimited.</li>
-          <li>Your products have been exposed on our search engine.</li>
+          <li>Priority Ranking (3rd)</li>
+          <li>Product Posting (Unlimited)</li>
         </ul>
         <div class="footer-button-container">
-          <a href="/for-supplier" class="table-button">{{ $t('pricing.getStarted') }}</a>
+          <a href="/for-supplier" class="table-button">{{ $t('pricing.select') }}</a>
         </div>
       </section>
 
       <!-- Standard -->
       <section class="pricing-table">
         <h2 class="table-title">{{ $t('pricing.standard') }}</h2>
-        <p class="table-pricing">$ 100 / year</p>
+        <p class="table-pricing">$1,000 / year</p>
         <ul>
-          <li>Upload product unlimited.</li>
-          <li>Your products have been exposed on our search engine.</li>
+          <li>Priority Ranking (2nd)</li>
+          <li>Product Posting (Unlimited)</li>
+          <li>Access to Buyer Information</li>
+          <li>Verified Icon</li>
+          <li>Personalized Customer Service</li>
         </ul>
         <div class="footer-button-container">
-          <a class="table-button">{{ $t('pricing.comingSoon') }}</a>
+          <a class="table-button">{{ $t('pricing.select') }}</a>
         </div>
       </section>
 
       <!-- Premium -->
       <section class="pricing-table">
         <h2 class="table-title">{{ $t('pricing.premium') }}</h2>
-        <p class="table-pricing">{{ $t('pricing.tellMe') }}</p>
+        <p class="table-pricing"> Coming Soon </p>
         <ul>
-          We are prepare for better service for you to discover reliable buyers.
+          <li>Priority Ranking (1st)</li>
+          <li>Product Posting (Unlimited)</li>
+          <li>Access to Buyer Information</li>
+          <li>Verified Icon</li>
+          <li>Personalized Customer Service</li>
         </ul>
+        Please tell us what you need.
         <div class="footer-button-container">
-          <a class="table-button button-black">{{ $t('pricing.comingSoon') }}</a>
+          <a class="table-button">{{ $t('pricing.contact') }}</a>
         </div>
       </section>
     </div>
@@ -47,81 +55,88 @@
 </template>
 
 <script>
-  export default {
-    head () {
-      return {
-        title: this.$t('pricing.title'),
-        link: [
-          { hid: 'canonical', rel: 'canonical', href: `https://www.factoryhunt.com/pricing` }
-        ]
-      }
+export default {
+  head() {
+    return {
+      title: this.$t('pricing.title'),
+      link: [{ hid: 'canonical', rel: 'canonical', href: `https://www.factoryhunt.com/pricing` }]
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  @import '~assets/css/index';
-  .sub-title {
-    margin: 0;
-  }
-  .pricing-table-container {
-    padding: 50px 0;
-    display: flex;
-    justify-content: space-around;
+@import '~assets/css/index';
+.sub-title {
+  margin: 0;
+}
+.pricing-table-container {
+  padding: 50px 0;
+  display: flex;
+  justify-content: space-around;
 
-    .pricing-table {
-      position: relative;
-      padding: 18px;
-      width: 30%;
-      min-height: 500px;
-      border: 1px solid #FFF;
-      border-radius: @border-radius;
-      transition: background-color 0.3s;
-      box-shadow: @box-shadow;
+  .pricing-table {
+    position: relative;
+    padding: 18px;
+    width: 30%;
+    min-height: 500px;
+    border: 1px solid #fff;
+    border-radius: @border-radius;
+    transition: background-color 0.3s;
+    box-shadow: @box-shadow;
 
-      .table-title {
-        font-size: 25px;
-        font-weight: @font-weight-bold;
-        margin-top: 5px;
-        padding-bottom: 14px;
-        border-bottom: @border-light-grey;
+    .table-title {
+      font-size: 25px;
+      font-weight: @font-weight-bold;
+      margin-top: 5px;
+      padding-bottom: 14px;
+      border-bottom: @border-light-grey;
+      text-align: center;
+    }
+
+    .table-pricing {
+      text-align: center;
+      font-size: @font-size-large;
+    }
+
+    ul {
+      li {
+        font-size: @font-size-medium;
+        font-weight: @font-weight-thin;
+        padding: 4px 0;
+        list-style: none;
       }
-
-      .table-pricing {
-        text-align: center;
-        font-size: @font-size-large;
+      li:before {
+        content: '\f00c';
+        font-family: 'FontAwesome';
+        float: left;
+        margin-left: -1.5em;
+        color: #0074d9;
       }
+    }
 
-      ul {
-        li {
-          font-size: @font-size-medium;
-          font-weight: @font-weight-thin;
-          padding: 4px 0;
-        }
-      }
+    .footer-button-container {
+      position: absolute;
+      text-align: center;
+      width: 100%;
+      bottom: 0;
+      left: 0;
+      padding: 20px 20px 40px 20px;
 
-      .footer-button-container {
-        position: absolute;
-        text-align: center;
-        width: 100%;
-        bottom: 0;
-        left: 0;
-        padding: 20px 20px 40px 20px;
+      .table-button {
+        padding: 12px 28px;
+        border-radius: @border-radius;
+        background-color: @color-orange;
+        color: @color-white;
+        font-size: 17px;
+        font-weight: @font-weight-medium;
+        text-decoration: none;
 
-        .table-button {
-          padding: 12px 28px;
-          border-radius: @border-radius;
-          background-color: @color-orange;
-          color: @color-white;
-          font-size: 17px;
-          font-weight: @font-weight-medium;
-          text-decoration: none;
-
-          &:hover {
-            background-color: @color-dark-orange;
-          }
+        &:hover {
+          background-color: @color-dark-orange;
         }
       }
     }
   }
+}
 </style>
