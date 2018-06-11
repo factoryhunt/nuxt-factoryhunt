c25226d5ea37777fa2592655cc1a5d45996a3c19
Peterkimzz <peterkimzz69@gmail.com>
2018-06-11 13:11:42 +0900

    Edit dashboard version

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -28,7 +28,7 @@
       <header class="header-container">
         <p class="sub-title">
           {{ $t('dashboard.welcome', {name: getAccountName}) }}
-          <span v-if="isTesterAccount"><br>This app is 0.7.7.5 version.</span>
+          <span v-if="isTesterAccount"><br>This app is 0.8 version.</span>
         </p>
       </header>
 
